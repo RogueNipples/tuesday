42fb165d9235f850dc6d8408d2b740f252aa2587
RogueNipples <soccerstriker57@gmail.com>
2017-06-22 13:18:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,36 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
   var botRegex1 = /^Tendies\?$/;
+  var botRegex2 = /^tendies\?$/;
+  var botRegex3 = /^Menu\?$/;
+  var botRegex4 = /^menu\?$/;
   
   if(request.text && botRegex1.test(request.text)) {
     this.res.writeHead(200);
     postMessage1();
+    this.res.end();
+  }
+  else if(request.text && botRegex2.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage1();
+    this.res.end();
+  }
+  else if(request.text && botRegex3.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage2("Monday: Whatever you want");
+    postMessage2("Tuesday: Tendies");
+    postMessage2("Wednesday: Subway");
+    postMessage2("Thursday: Arby's");
+    postMessage2("Friday: KFC");
+    this.res.end();
+  }
+  else if(request.text && botRegex4.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage2("Monday: Whatever you want");
+    postMessage2("Tuesday: Tendies");
+    postMessage2("Wednesday: Subway");
+    postMessage2("Thursday: Arby's");
+    postMessage2("Friday: KFC");
     this.res.end();
   }
   else {
@@ -50,6 +76,44 @@ else
 var butts = "It's the weekend, do whatever the fucc you want";
 }
         botResponse = butts;
+
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+//==================
+
+function postMessage1(input) {
+  var botResponse, options, body, botReq;
+
+        botResponse = input;
 
 
   options = {
