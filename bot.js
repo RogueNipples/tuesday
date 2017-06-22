d47863f389a0030853a62e8696eb952eed229448
RogueNipples <soccerstriker57@gmail.com>
2017-06-22 13:08:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
@@ -11,10 +10,12 @@ function respond() {
     this.res.writeHead(200);
     postMessage1();
     this.res.end();
+  }
   else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
+  }
   }
 }
 
@@ -25,7 +26,7 @@ function postMessage1() {
 
 var date = Date();
 var day = date.substr(0,3);
-else if (day == "Mon")
+if (day == "Mon")
 {
 var butts = "Nope, it's My Choice Monday";
 }
@@ -81,65 +82,6 @@ var butts = "It's the weekend, do whatever the fucc you want";
   });
   botReq.end(JSON.stringify(body));
 }
-
-//==========================================================================================
-function postMessage2(variable) {
-  var botResponse, options, body, botReq;
-  
-var m,n,o;	
-	
-    var n = variable.lastIndexOf("er");
-    if (variable.indexOf(" ", n) == -1) {
-    m = variable.length;
-    }
-    else {
-    var m = variable.indexOf(" ", n);
-    }
-    if (variable.lastIndexOf(" ", n) == -1) {
-    o = 0;
-    }
-    else {
-    var o = variable.lastIndexOf(" ", n);
-    }
-    var wordlength = m-o;
-    var substring = variable.substr(o,wordlength);
-    
-    var finalstring = substring.concat("? I barely know her!")
-	
-        botResponse = finalstring;
-
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-//==========================================================================================
-
 
 
 exports.respond = respond;
